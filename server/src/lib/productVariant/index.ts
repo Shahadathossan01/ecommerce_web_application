3c369658784e91315bb3641ead118fa754cba6f7
topu <topu@gmail.com>
2025-09-20 05:01:14 +0600

    update: create logic

--- server/src/lib/productVariant/index.ts
+++ server/src/lib/productVariant/index.ts
@@ -3,6 +3,7 @@ import {
   IProductVariant,
   ProductVariantInput,
 } from "@src/types/product_variant";
+import error from "@src/utils/error";
 
 const create = async ({
   color,
@@ -10,11 +11,25 @@ const create = async ({
   product_id,
   size,
 }: ProductVariantInput): Promise<IProductVariant> => {
+  const existingProductVariant = await ProductVariant.find({
+    product: product_id,
+  }).lean();
+
+  if (
+    existingProductVariant.some(
+      (variant) =>
+        variant.color === color.toUpperCase() &&
+        variant.size === size.toUpperCase()
+    )
+  ) {
+    throw error(400, "Bad Request", "Color and size already used");
+  }
+
   const productVariant = new ProductVariant({
-    color,
+    color: color.toUpperCase(),
     images,
     product: product_id,
-    size,
+    size: size.toUpperCase(),
   });
 
   await productVariant.save();
